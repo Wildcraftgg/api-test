cba3ab9d05a370dc326f1f65a8fe6f6cff80bcfd
Wildcraftgg <122021315+Wildcraftgg@users.noreply.github.com>
2024-10-03 20:38:06 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,34 +1,40 @@
-// Import Express and Axios
-const express = require('express');
 const axios = require('axios');
-
-// Create an Express application
+const express = require("express")
 const app = express();
 
-// Define a port for the server to listen on
 const PORT = process.env.PORT || 3000;
-
-// Define a route that fetches data from another API
-app.get('/getData', async (req, res) => {
+app.get('/getData',async(req,res) => {
   try {
-    // Fetch data from an external API (replace with your target API URL)
-    const response = await axios.get('https://assetdelivery.roblox.com/v2/asset/?id=307181070');
-
-    // Send the fetched data as the response in JSON format
+    const response = await axios.get('https://assetdelivery.roblox.com/v2/asset/?id=307181070', {
+      headers: {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'cache-control': 'max-age=0',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+      }
+    });
     res.json({
-      status: 'success',
-      data: response.data
-    });
+        status: 'success',
+        data: response.data
+    })
   } catch (error) {
-    // Handle errors (e.g., if the external API fails)
     res.status(500).json({
-      status: 'error',
-      message: 'Failed to fetch data from the external API',
-    });
+        status: 'error',
+        message: 'Error: '+error
+    })
   }
-});
+})
 
-// Start the server and listen on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+ console.log('Server running');
 });

b9e2d1226f640fad9085ced4ea14679d93cf0ad1
Wildcraftgg <122021315+Wildcraftgg@users.noreply.github.com>
2024-10-04 11:54:55 +0530

    Update api.js

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ app.get('/api/asset/:id', async (req, res) => {
   const assetId = req.params.id; // Get the asset ID from the URL parameter
 
   try {
-    const response = await axios.get(`https://assetdelivery.roblox.com/v2/asset/?id=${assetId}`, {
+    const response = await axios.get(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${assetId}`, {
       headers: {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-encoding': 'gzip, deflate, br, zstd',
